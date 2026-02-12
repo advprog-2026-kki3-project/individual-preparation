ad49b81424088049d3c239bf1c174942e9cb6032
RafaMiyauchi <rafamiyauchi@gmail.com>
2026-02-12 15:39:18 +0700

    Implemented the dot product method within VectorUtility

--- src/main/java/com/example/individualprep/service/VectorUtility.java
+++ src/main/java/com/example/individualprep/service/VectorUtility.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class VectorUtility {
-    
+
     public double[] add(double[] v1, double[] v2) {
         // TODO: Implement me properly!
         return new double[] { 0.0, 0.0, 0.0 };
@@ -19,12 +19,19 @@ public class VectorUtility {
         // TODO: Implement me properly!
         return new double[] { 0.0, 0.0, 0.0 };
     }
-    
+
     public double dotProduct(double[] v1, double[] v2) {
-        // TODO: Implement me properly!
-        return 0.0;
+        if (v1.length != v2.length) {
+            throw new IllegalArgumentException("Vector lengths has to be equal");
+        }
+
+        double sum = 0.0;
+        for (int i = 0; i < v1.length; i++) {
+            sum += v1[i] * v2[i];
+        }
+        return sum;
     }
-    
+
     public double norm(double[] v1) {
         // TODO: Implement me properly!
         return 0.0;
